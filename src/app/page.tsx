bb762ed848c921e234112a872a87e15fd02046fe
Petar <petar.kovacovski@gmail.com>
2025-08-09 12:04:59 +0200

    pet message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,8 @@ export default async function Home() {
           blockType={blockMap["text1"]?.blockType ?? "TEXT"}
         />
 
+        <h2>TIP</h2>
+
         <EditableBlock
           relUrl="/"
           blockKey="text2"
