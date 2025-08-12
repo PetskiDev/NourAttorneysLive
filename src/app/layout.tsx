5cab1cf10a36dd052038b99ef4de1da54204d02f
Petar <petar.kovacovski@gmail.com>
2025-08-12 12:39:01 +0200

    Font Manrope Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,23 @@
 import "~/styles/globals.css";
 import ClientShell from "../components/ClientShell";
+import { Manrope } from "next/font/google";
+import { Inter } from "next/font/google";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-manrope",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${manrope.variable} ${inter.variable}`}>
       <body>
         <ClientShell>{children}</ClientShell>
       </body>
